b07f2b291e4ee238d75ec549fe013cff9b77a4f8
jvgreen <jonahgreen81@gmail.com>
2021-11-09 14:49:01 -0700

    End of section 6

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -15,6 +15,7 @@ interface Props {
     closeForm: () => void;
     createOrEdit: (activity: Activity) => void;
     deleteActivity: (id: string) => void;
+    submitting: boolean;
 }
 
 export default function ActivityDashboard({activities, 
@@ -25,7 +26,8 @@ export default function ActivityDashboard({activities,
                                           openForm, 
                                           closeForm, 
                                           createOrEdit, 
-                                          deleteActivity}: Props) {
+                                          deleteActivity,
+                                          submitting}: Props) {
     return (
         <Grid>
             <Grid.Column width='10'>
@@ -33,6 +35,7 @@ export default function ActivityDashboard({activities,
                     activities={activities} 
                     selectActivity={selectActivity}
                     deleteActivity={deleteActivity} 
+                    submitting={submitting}
                 />
             </Grid.Column>
             <Grid.Column width='6'>
@@ -47,6 +50,7 @@ export default function ActivityDashboard({activities,
                     closeForm={closeForm} 
                     activity={selectedActivity} 
                     createOrEdit={createOrEdit}
+                    submitting={submitting}
                 /> }
             </Grid.Column>
         </Grid>
--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -6,8 +6,12 @@ interface Props {
     activity: Activity | undefined;
     closeForm: () => void;
     createOrEdit: (activity: Activity) => void;
+    submitting: boolean;
 }
-export default function ActivityFrorm({activity: selectedActivity, closeForm, createOrEdit}: Props) {
+export default function ActivityFrorm({activity: selectedActivity, 
+                                       closeForm, 
+                                       createOrEdit,
+                                       submitting}: Props) {
 
     const initialState = selectedActivity ?? {
         id: '',
@@ -39,7 +43,7 @@ export default function ActivityFrorm({activity: selectedActivity, closeForm, cr
                 <Form.Input placeholder='Date' value={activity.date} name='date' onChange={handleInputChange}/>
                 <Form.Input placeholder='City' value={activity.city} name='city' onChange={handleInputChange}/>
                 <Form.Input placeholder='Venue' value={activity.venue} name='venue' onChange={handleInputChange}/>
-                <Button floated='right' positive type='submit' content='Submit'/>
+                <Button loading={submitting} floated='right' positive type='submit' content='Submit'/>
                 <Button onClick={closeForm} floated='right' type='button' content='Cancel'/>
             </Form>
         </Segment>
